34b6f41adbc46fa25dab497ce1a6ff94791a0acc
Tom Martin <tom@tpgmartin.com>
2015-04-13 16:22:31 +0100

    Add basic project structure

---
+++ app.js
@@ -0,0 +1,7 @@
+// Module dependencies go here
+var express = require('express');
+var http = require('http');
+var path = require('path');
+// Reference to config goes here
+var app = express();
+// References to views go here
